a89e8096e1e567ca9669ffc268dbfaa3db237bbb
jd <jdsanchez2011@gmail.com>
2024-11-22 15:57:35 -0700

    show file size & refactor file to use signals

--- src/app/upload/upload-clip/upload-clip.component.ts
+++ src/app/upload/upload-clip/upload-clip.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,6 +6,7 @@ import { catchError, of, switchMap, tap } from 'rxjs';
 import { UploadService } from '../upload.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HttpEventType } from '@angular/common/http';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-upload-clip',
@@ -14,13 +15,17 @@ import { HttpEventType } from '@angular/common/http';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    DecimalPipe
   ],
   templateUrl: './upload-clip.component.html',
   styleUrl: './upload-clip.component.scss'
 })
 export class UploadClipComponent {
-  fileToUpload: File | null = null;
+  fileToUpload: WritableSignal<File | null> = signal(null);
+  fileSizeInBytes: Signal<number> = computed(() => this.fileToUpload()?.size ?? 0);
+  fileName: Signal<string> = computed(() => this.fileToUpload()?.name ?? '');
+
   preview: any;
   isUploading: boolean = false;
   uploadProgress = signal(0);
@@ -30,7 +35,7 @@ export class UploadClipComponent {
   handleFileInput(event: Event) {
     let fileList: FileList | null = (event.currentTarget as HTMLInputElement).files;
     if (fileList && fileList.length > 0) {
-      this.fileToUpload = fileList[0];
+      this.fileToUpload.set(fileList[0]);
 
       let reader: FileReader = new FileReader();
 
@@ -38,12 +43,12 @@ export class UploadClipComponent {
         this.preview = reader.result as string;
       };
 
-      reader.readAsDataURL(this.fileToUpload);
+      reader.readAsDataURL(this.fileToUpload()!);
     }
   }
 
   uploadFile() {
-    if (this.fileToUpload == null) {
+    if (this.fileToUpload() == null) {
       console.error('No file selected.')
       return;
     }
@@ -51,9 +56,9 @@ export class UploadClipComponent {
     this.isUploading = true;
     this.uploadProgress.set(0);
 
-    this.uploadService.callApiGateway({ fileName: this.fileToUpload.name })
+    this.uploadService.callApiGateway({ fileName: this.fileToUpload()!.name })
       .pipe(
-        switchMap((uploadUrl) => this.uploadService.uploadFile(uploadUrl, this.fileToUpload!)),
+        switchMap((uploadUrl) => this.uploadService.uploadFile(uploadUrl, this.fileToUpload()!)),
         tap(event => {
           if (event.type === HttpEventType.UploadProgress) {
             this.uploadProgress.set((event.loaded / event.total!) * 100);
