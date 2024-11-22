c1e80e19e09a4973398a668053529464f0be5d32
jd <jdsanchez2011@gmail.com>
2024-11-21 20:29:22 -0700

    remove route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'upload', loadComponent: () => import('./upload/upload-clip/upload-clip.component').then(mod => mod.UploadClipComponent) },
-    { path: '**', redirectTo: 'upload' }
+    { path: '', loadComponent: () => import('./upload/upload-clip/upload-clip.component').then(mod => mod.UploadClipComponent) },
+    { path: '**', redirectTo: '' }
 ];
